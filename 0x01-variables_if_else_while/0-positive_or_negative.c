4bf9b5b0cb1f5dd678740b81ac9feab7745b26f8
NKGachoki <njambagachoki@gmail.com>
2022-03-14 09:51:38 -0700

    correction to task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
+/**
+ * main - assigns a random number to int n eact time it is executed
+ *
+ * return 0 (success)
+ */
 
-/* betty style doc for function main goes there */
 int main(void)
 {
 	int n;
