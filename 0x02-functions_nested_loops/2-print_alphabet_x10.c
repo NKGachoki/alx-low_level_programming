cbbfc55e7e127b32def6cc925a7020d8b01e45de
NKGachoki <njambagachoki@gmail.com>
2022-03-16 06:01:21 -0700

    answer to task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabet in lower case 10x
+ * followed by new line
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	char a = 0, b;
+
+	while (a < 10)
+	{
+		b = 'a';
+		
+		while (b <= 'z')
+		{
+			_putchar(b);
+			b++;
+		}
+		_putchar('\n');
+		a++;
+	}
+	
+}
