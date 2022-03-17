26dc283f37140a3c0692674a3ab4067a625cbf19
NKGachoki <njambagachoki@gmail.com>
2022-03-17 04:37:06 -0700

    answer to task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for upper case character
+ *@c: parameter
+ * Return: 1 if char is uppercase or 0 otherwise
+ */
+
+int _isupper(int c)
+{
+
+	if (c <= 'Z' && c >= 'A')
+	_putchar(c)
+}
