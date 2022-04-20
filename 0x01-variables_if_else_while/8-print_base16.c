58768e2117843a0c3173c1c2881b6b322e605765
NKGachoki <njambagachoki@gmail.com>
2022-04-20 06:59:41 -0700

    correction to task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,11 +8,14 @@
 
 int main(void)
 {
-	for (char num = '0'; num <= '9'; num++)
+	char num;
+	char letter;
+
+	for (num = '0'; num <= '9'; num++)
 	{
 	putchar(num);
 	}
-	for (char letter = 'a'; letter <= 'f'; letter++)
+	for (letter = 'a'; letter <= 'f'; letter++)
 	{
 	putchar(letter);
 	}
