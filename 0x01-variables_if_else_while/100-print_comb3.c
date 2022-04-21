8f80fee17878fbd7a5779f8e68602717732cd0ca
NKGachoki <njambagachoki@gmail.com>
2022-04-21 03:45:32 -0700

    Answer to task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - prints the smallest combo of 2 digits
+ * without reusing them in the sequence
+ * Return: Always 0 (success)
+ */
+ 
+int main(void)
+{
+	int n, m;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		for (m = '0'; m <= '9'; m++)
+		{
+			if ((n != m )&&(n < m))
+			{
+				putchar(n);
+				putchar(m);
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
