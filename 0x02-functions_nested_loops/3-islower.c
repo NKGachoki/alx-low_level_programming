51e7f3fd1d9298895a38b3d97f2823972abdcfe3
NKGachoki <njambagachoki@gmail.com>
2022-04-25 04:37:23 -0700

    correction tot ask 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,18 +2,17 @@
 
 /**
  * _islower - checks for lower case characters
- *
+ *@c: the char to be checked
  * Return: 1 if c is lowercase, returns 0 otherwise
  */
 
 int _islower(int c)
 {
-	
 	if (c <= 'z' && c >= 'a')
 	{
 		return (1);
 	}
-	else 
+	else
 	{
 		return (0);
 	}
