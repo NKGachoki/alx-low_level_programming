02724f259b087a68bb14f89dcb8947519e1246b6
NKGachoki <njambagachoki@gmail.com>
2022-04-28 03:08:51 -0700

    Correction to task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,14 +14,16 @@ int i;
 
 for (i = 1; i <= 100; i++)
 {
-	if (i % 3 == 0)
-	printf("Fizz\n");
-	else if (i % 5 == 0)
-	printf("Buzz\n");
+	if ((i % 3 == 0) && (i % 5 != 0))
+	printf(" Fizz");
+	else if ((i % 5 == 0) && (i % 3 != 0))
+	printf(" Buzz");
 	else if ((i % 3 == 0) && (i % 5 == 0))
-	printf("FizzBuzz\n");
+	printf(" FizzBuzz");
+	else if (i == 1)
+	printf("%d", i);
 	else
-	printf("%d\n", i);
+	printf(" %d", i);
 }
 return (0);
 }
